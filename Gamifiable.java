5f0dd0ccb868dd6871c31b98e899464421a715ee
ColdShadow01 <185036448+ColdShadow01@users.noreply.github.com>
2026-05-31 07:09:03 +0800

    Track 1: Added Core User model, file I/O operations, and custom boundaries.

--- Gamifiable.java
+++ Gamifiable.java
@@ -2,7 +2,7 @@
 //Tester: 
 //Description: Interface defining the required gamification methods.
 public interface Gamifiable {
-  void addPoints(int baseScore);
-  void addPoints(int baseScore, int timerBonus); //Demonstrates Method Overloading
+  void addPoints(int points);
+  void addPoints(int points, int bonus); //Demonstrates Method Overloading
   void awardBadge(int totalScore);
 }
