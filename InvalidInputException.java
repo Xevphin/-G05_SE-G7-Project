842c788c16dd388963f0e2840bc7413506eeed17
ColdShadow01 <185036448+ColdShadow01@users.noreply.github.com>
2026-05-31 12:25:50 +0800

    Delete InvalidInputException.java

--- InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-// Creator: 
-// Tester: 
-// Description: Custom exception to catch invalid user inputs during the quiz
-public class InvalidInputException extends Exception {
-  public InvalidInputException(String message) {
-    super(message);
-  }
-}
