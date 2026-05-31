b8d841bdd682c2681a138d8fec16bf7b962dab86
ColdShadow01 <185036448+ColdShadow01@users.noreply.github.com>
2026-05-31 12:26:01 +0800

    Delete QuizNotFoundException.java

--- QuizNotFoundException.java
+++
@@ -1,8 +0,0 @@
-//Creator: 
-//Testor: 
-//Description: Custom exception to catch errors if quiz data fails to load.
-public class QuizNotFoundException extends Exception {
-    public QuizNotFoundException(String message) {
-        super(message);
-    }
-}
